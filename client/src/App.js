cd1313438bc1549c91f0b91d60eb04063f60a19c
adamadlawrence <adam.ad.lawrence@gmail.com>
2017-12-07 03:17:22 -0500

    highcharts integration

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,17 @@
 import React, { Component } from 'react';
+import DonutChart from './DonutChart'
 import './App.css';
 
 class App extends Component {
   state = { data: {} }
 
   componentDidMount() {
-    // fetch('/poloniexData/ticker')
-    //   .then(res => res.json())
-    //   .then(users => this.setState({ users }));
-
     fetch('/poloniexData/ticker')
       .then(res => res.json())
       .then(data => {
         console.log(data);
         this.setState({ data });
       });
-    
-    // console.log(res.json());
   }
 
   render() {
@@ -24,7 +19,8 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Users</h1>
-        <div><pre>{JSON.stringify(this.state.data, null, 2) }</pre></div>
+        <DonutChart />
+        <div><pre>{JSON.stringify(this.state.data, null, 2)}</pre></div>
         {/* {this.state.data} */}
         {/* {this.state.data.map(pair =>
           <div key={pair}>{pair}</div>
---
+++ client/src/DonutChart.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+const ReactHighcharts = require('react-highcharts');
+
+class DonutChart extends Component {
+
+  // constructor(props) {
+  //   super(props)
+  // }
+  
+  state = {
+    blah: 'hi',
+
+    config: {
+      chart: {
+        renderTo: 'container',
+        type: 'pie'
+      },
+      title: {
+        text: 'Digital Assset Holdings'
+      },
+      yAxis: {
+        title: {
+          text: 'Total percent market share'
+        }
+      },
+      plotOptions: {
+        pie: {
+          shadow: false
+        }
+      },
+      tooltip: {
+        formatter: function () {
+          return parseInt(this.point.percentage) + '%' + `<br>${this.point.y} ${this.point.name}`;
+        }
+      },
+      series: [{
+        name: 'Browsers',
+        data: this.updatePortfolio(),
+        size: '90%',
+        innerSize: '60%',
+        showInLegend: true,
+        dataLabels: {
+          enabled: true
+        }
+      }]
+    }
+  }
+  
+  updatePortfolio() {
+    // poll data
+    console.log('hi',this)
+    return [['ZEC', 0.06138262],
+    ['DASH', 0.00000362],
+    ['BTCD', 0.07855733],
+    ['DASH', 0.00000362],
+    ['MAID', .1354355480],
+    ['REP', 0.00003815],
+    ['LSK', 0.20641178],
+    ['ZEC', 0.06138262],
+    ['XRP', .1730111802],
+    ['USDT', .18860279],
+    ['BTC', 0.00099013],
+    ['STEEM', 0.00039982]]
+    // update highchart state with data
+  }
+
+  addHolding = () => {
+    // console.log(this);
+    this.state.config.series[0].data.push(['hi', 0.444]);
+    // this.setState({this.state.config.series[0].data: this.config[series[0].data.push(['hi', 0.444])]})
+    // this.setState{ config.series[0].data.push(['Blah', .52342])}
+  }
+
+  render() {
+    // updatePortfolio()]
+    // this.addHolding()
+    console.log(this.state)
+    return (
+      <div>
+        <ReactHighcharts config={this.state.config} />
+        <button onClick={this.addHolding}/>
+      </div>
+    )
+  }
+}
+
+export default DonutChart
