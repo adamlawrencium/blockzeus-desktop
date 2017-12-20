97cdf4ec4a99fe5c725d94c1c8e977ff36b544ed
adamadlawrence <adam.ad.lawrence@gmail.com>
2017-12-19 22:47:20 -0800

    edit for heroku deployment

--- app.js
+++ app.js
@@ -45,11 +45,17 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/', index);
 app.use('/users', users);
 app.use('/poloniexData', poloniexData);
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
