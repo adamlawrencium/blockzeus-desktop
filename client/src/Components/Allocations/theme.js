443928704e5e2f9e46f43eedf34fd15695805fef
adamadlawrence <adam.ad.lawrence@gmail.com>
2018-01-05 15:47:42 -0800

    Chart theming and css

---
+++ client/src/Components/Allocations/theme.js
@@ -0,0 +1,13 @@
+export const theme = {
+  chart: {
+    style: {
+      fontFamily: 'Barlow, sans-serif',
+    },
+  },
+  tooltip: {
+    backgroundColor: '#482D62',
+    style: {
+      color: '#F0F0F0',
+    },
+  },
+};
