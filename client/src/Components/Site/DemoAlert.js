9fe14e7d5b4b5bb6b2ba75374efb86ae5cff778e
adamadlawrence <adam.ad.lawrence@gmail.com>
2018-01-21 02:31:24 -0500

    styling changes

--- client/src/Components/Site/DemoAlert.js
+++ client/src/Components/Site/DemoAlert.js
@@ -4,12 +4,12 @@ class DemoAlert extends Component {
   render() {
     return (
       <div className="alert alert-info alert-dismissible fade show" role="alert">
-            This is a demo — <a href="" className="alert-link">sign up</a> and connect to Poloniex to start managing your portfolio.
-            And psst, leave feedback <a href="https://goo.gl/forms/XcIs6gZS4qBphFeA3" target="_blank" className="alert-link">here</a> :)
-            <button type="button" className="close" data-dismiss="alert" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
+        This is a demo — <a href="" className="alert-link">sign up</a> and connect to Poloniex to start managing your portfolio.
+        And psst, leave feedback <a href="https://goo.gl/forms/XcIs6gZS4qBphFeA3" target="_blank" className="alert-link">here</a> :)
+        <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
     );
   }
 }
