0939c56993d64b8c2aea21984958641cb057835c
adamadlawrence <adam.ad.lawrence@gmail.com>
2018-02-04 23:18:50 -0500

    lets try this again

--- server/app.js
+++ server/app.js
@@ -54,7 +54,7 @@ app.use('/poloniex/', poloniex);
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/client/build/index.html`));
+  res.sendFile(path.join(`../client/build/index.html`));
 });
 
 // catch 404 and forward to error handler
