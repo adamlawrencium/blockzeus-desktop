5c0c1be8f7e6cadddc2a00ed7f59d60fdb1ee66e
adamadlawrence <adam.ad.lawrence@gmail.com>
2018-02-04 23:58:16 -0500

    added landing page, now testing on heroku

---
+++ routes/landingPage/landingPage.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+module.exports = router;
