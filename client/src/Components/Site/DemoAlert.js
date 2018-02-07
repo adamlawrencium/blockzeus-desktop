77636bc2d8cc8c21b7c00fb60c717528088578bf
adamadlawrence <adam.ad.lawrence@gmail.com>
2018-02-07 00:30:49 -0500

    re-org'd page, added signup link to demo

--- client/src/Components/Site/DemoAlert.js
+++ client/src/Components/Site/DemoAlert.js
@@ -4,7 +4,7 @@ class DemoAlert extends Component {
   render() {
     return (
       <div className="alert alert-info alert-dismissible fade show" role="alert">
-        This is a demo — <a href="" className="alert-link">sign up</a> and connect to Poloniex to start managing your portfolio.
+        This is a demo — <a href="https://blockzeus.com" className="alert-link">sign up</a> and connect to Poloniex to start managing your portfolio.
         And psst, leave feedback <a href="https://goo.gl/forms/XcIs6gZS4qBphFeA3" target="_blank" className="alert-link">here</a> :)
         <button type="button" className="close" data-dismiss="alert" aria-label="Close">
           <span aria-hidden="true">&times;</span>
