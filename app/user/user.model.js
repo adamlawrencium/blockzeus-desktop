1571671ca4a12696d2712c8cf718c9009498c9d9
adamadlawrence <adam.ad.lawrence@gmail.com>
2018-02-11 13:04:37 -0500

    large folder restructure, basic auth set up

---
+++ app/user/user.model.js
@@ -0,0 +1,64 @@
+const crypto = require('crypto');
+const bcrypt = require('bcrypt-nodejs');
+const mongoose = require('mongoose');
+
+const schemaOptions = {
+  timestamps: true,
+  toJSON: {
+    virtuals: true,
+  },
+};
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
+  gender: String,
+  location: String,
+  website: String,
+  picture: String,
+  facebook: String,
+  twitter: String,
+  google: String,
+  github: String,
+  vk: String,
+}, schemaOptions);
+
+userSchema.pre('save', function (next) {
+  const user = this;
+  if (!user.isModified('password')) { return next(); }
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function (password, cb) {
+  bcrypt.compare(password, this.password, (err, isMatch) => {
+    cb(err, isMatch);
+  });
+};
+
+userSchema.virtual('gravatar').get(function () {
+  if (!this.get('email')) {
+    return 'https://gravatar.com/avatar/?s=200&d=retro';
+  }
+  const md5 = crypto.createHash('md5').update(this.get('email')).digest('hex');
+  return `https://gravatar.com/avatar/${md5}?s=200&d=retro`;
+});
+
+userSchema.options.toJSON = {
+  transform(doc, ret, options) {
+    delete ret.password;
+    delete ret.passwordResetToken;
+    delete ret.passwordResetExpires;
+  },
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
