290202fb200fe1fe4f1afbf1ef46b64b7af26bf2
adamadlawrence <adam.ad.lawrence@gmail.com>
2018-02-23 15:01:03 -0500

    removed old redux code

--- client/src/index.js
+++ client/src/index.js
@@ -1,31 +1,11 @@
-// import React from 'react';
 import ReactDOM from 'react-dom';
-// import { Provider } from 'react-redux';
-// import { Router, browserHistory } from 'react-router-dom';
-// import configureStore from './store/configureStore';
-// import getRoutes from './routes';
-
-// const store = configureStore(window.INITIAL_STATE);
-
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>,
-//   document.getElementById('app'),
-// );
-
-
 import React from 'react';
-// import { render } from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import { Provider } from 'react-redux';
 
-import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
-
-
 import { store, configureFakeBackend } from './_helpers';
 import { App } from './App';
-// import Dashboard from './Components/Dashboard';
 
 // setup fake backend
 configureFakeBackend();
@@ -33,7 +13,6 @@ configureFakeBackend();
 ReactDOM.render(
   <Provider store={store}>
     <App />
-    {/* <Dashboard /> */}
   </Provider>,
   document.getElementById('root'),
 );
